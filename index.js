24b7dd0966bd98995df2132ce1c37f9ac6146237
Hesham Adel <hadel@sharpmobile.ca>
2017-03-12 20:05:12 +0200

    Add shouldExist validation

--- index.js
+++ index.js
@@ -11,11 +11,32 @@ const MachinePackRules = function(rules){
     this.rules = rules; 
 };
 
-MachinePackRules.prototype.isRulesEnforced = function(inputs) {
+MachinePackRules.prototype.isRulesEnforcedOn = function(inputs) {
+    var flag = true;
+    console.log('\t>> Rules: ', this.rules);
     if (!this.rules){
         console.error('No rules provided');
-        return false; 
+        flag = false; 
     }
+
+    this.rules.forEach(function(rule) {
+        if (rule.hasOwnProperty('shouldExist')){
+            if (rule.shouldExist.constructor !== Array){
+                console.error('shouldExist must be an array'); 
+                flag = false;
+            } else {
+               rule.shouldExist.forEach(function(shouldExistInput){
+                   if (!inputs.hasOwnProperty(shouldExistInput)){
+                       console.log('\t>> Not Found: ', shouldExistInput);
+                       flag = false; 
+                   } else {
+                       console.log('\t>> Found: ' + shouldExistInput); 
+                   }
+               });
+            }
+        }
+    });
+    return flag;
 };
 
 
